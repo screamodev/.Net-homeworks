6d9500e5218059c17c811d9a28ad4c2316a0e807
Arsenii Yurchenko <arseniiyurchenko@Arseniis-MacBook-Air.local>
2024-04-29 16:58:53 +0300

    Add homework

---
+++ module3hw5/module3hw5/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    private static async Task<string> ReadFile(string filename)
+    {
+        var file = await File.ReadAllLinesAsync(filename);
+        return file[0];
+    }
+    
+    private static async void MethodsExecutor()
+    {
+      var hello = await ReadFile("../../../hello.txt");
+      var world = await ReadFile("../../../world.txt");
+
+      Console.WriteLine(hello + world);
+    }
+    
+    public static void Main()
+    {
+        MethodsExecutor();
+    }
+}
